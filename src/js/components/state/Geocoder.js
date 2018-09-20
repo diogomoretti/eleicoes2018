ac43c6f248dddc78acd1f49557d9f5494a4db717
luan0ap <luanap.kun@gmail.com>
2018-09-20 15:08:40 -0300

    changed arg in geocoder

--- src/js/components/state/Geocoder.js
+++ src/js/components/state/Geocoder.js
@@ -2,7 +2,7 @@ import Google from 'google-maps'
 
 Google.VERSION = 'weekly'
 
-export const Geocoder = () => 
+export const Geocoder = () =>
   new Promise((res) => {
-    Google.load((google) => res(google.maps.Geocoder()))
+    Google.load(({ maps }) => res(maps.Geocoder()))
   })
