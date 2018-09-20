86e36aaa37e907f23fd683be4351f36657d5762f
luan0ap <luanap.kun@gmail.com>
2018-09-20 14:56:52 -0300

    created currentLocation function

---
+++ src/js/components/state/currentLocation.js
@@ -0,0 +1,13 @@
+const location = () => {
+  return new Promise((res, rej) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(({ coords }) => {
+        res({
+          lat: parseFloat(coords.latitude),
+          lng: parseFloat(coords.longitude)
+        })
+      })
+    }
+    return rej(new Error('Navigator Geolocation not suported in your browser'))
+  })
+}
